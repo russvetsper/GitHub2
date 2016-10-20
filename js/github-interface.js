504d813bc4c303fb1148163bcd7baca57efe9a64
russvetsper <russvetsper@gmail.com>
2016-10-20 00:44:17 -0700

    add api key to .env file

---
+++ js/github-interface.js
@@ -0,0 +1,15 @@
+var Repo = require("./../js/github.js").repoModule;
+
+
+
+
+$(document).ready(function() {
+  $('#getName').click(function() {
+    var name = $('#name').val();
+    $('#name').val("");
+
+    var repoObject = new Repo();
+    repoObject.getRepos(name);
+    
+  });
+});
